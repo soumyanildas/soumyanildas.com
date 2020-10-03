89862f3de720299a660b8cf33b52da6c2b11eda0
Soumyanil Das <soumyanil666@gmail.com>
2020-10-03 20:54:32 +0530

    added light and dark mode

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 (function () {
+  document.getElementById('toggle-box-checkbox').checked = false;
+  toggleMode(false);
+  if (JSON.parse(localStorage.getItem('dark'))) {
+    document.getElementById('toggle-box-checkbox').checked = true;
+    toggleMode(true);
+  }
   const greetings = ['Hello', 'Hallo', 'Bonjour', 'Olá', 'Hola', 'Namaste', 'Nomoshkar', 'Konnichiwa', 'Ciao'];
   const random = Math.floor(Math.random() * greetings.length);
   document.querySelector('.greetings-message').innerHTML = greetings[random];
 })();
+
+document.getElementById('toggle-box-checkbox').addEventListener('change', function () {
+  const isChecked = this.checked;
+  localStorage.setItem('dark', JSON.stringify(isChecked));
+  toggleMode(isChecked);
+});
+
+function toggleMode(isDark) {
+  if (isDark) {
+    document.body.classList.remove('light');
+    document.body.classList.add('dark');
+  } else {
+    document.body.classList.remove('dark');
+    document.body.classList.add('light');
+  }
+}
